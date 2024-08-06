44c97b09753c1cf2f9661a9902dd97c0836b53bd
TomLayan1 <afolayantomisin1919@gmail.com>
2024-08-06 08:13:48 +0100

    Fetched the testimonial data using axios

--- car-rental/src/Component/AboutUs.jsx
+++ car-rental/src/Component/AboutUs.jsx
@@ -6,22 +6,22 @@ const AboutUs = () => {
   return (
     <div className='py-16 h-[100vh] overflow-hidden'>
       <motion.h2
-        initial={{ translateY: 100, scale: 0.5 }}
+        initial={{ translateY: 140, scale: 0.5 }}
         whileInView={{ translateY: 0, scale: 1 }}
-        transition={{ duration: 1.5, delay: 0.1 }}
+        transition={{ duration: 1, delay: 0.1 }}
         className='text-center text-3xl font-bold text-primaryColor mb-20'>About Us</motion.h2>
       <div className='flex items-center'>
         <motion.div
           initial={{ translateX: -400 }}
           whileInView={{ translateX: 0 }}
-          transition={{ duration: 2, delay: 0.2 }}
+          transition={{ duration: 1, delay: 0.2 }}
           className='w-[50%] max-h-[300px] flex items-center'>
           <img src={blackSuv} alt='black suv'/>
         </motion.div>
         <motion.div
           initial={{ translateX: 400 }}
           whileInView={{ translateX: 0 }}
-          transition={{ duration: 2, delay: 0.2 }}
+          transition={{ duration: 1, delay: 0.2 }}
           className='w-[50%] p-6'>
           <p className='text-[14px] mb-5'>
             Welcome to the premier destination for luxury car rentals. We pride ourselves on delivering an exceptional driving experience, combining elegance, comfort, and performance to meet the highest standards of our discerning clientele.
